e06fd906909be6877942c8c603fac2c0a2f91a15
Pham Minh Son <sonpm4@ghtk.co>
2024-05-13 14:25:01 +0700

    commit

--- shop/models/request.py
+++ shop/models/request.py
@@ -165,18 +165,19 @@ class Request(models.Model): # similar to "cart"
         return cart_items
     
     def request_custom_id(self):
-        unix_timestamp = datetime.datetime.timestamp(datetime.datetime(self.date))*1000
         car_kw = str(self.car).split()
         carinfo = Car.get_car_info_for_cart(car_kw[0],car_kw[1],car_kw[2])
-        return self.customer + "_" + carinfo.get_brand() + "_" + carinfo.get_model() + "_" + str(carinfo.get_year()) + "_" + str(int(unix_timestamp))
+        return self.customer + "_" + carinfo.get_brand() + "_" + carinfo.get_model() + "_" + str(carinfo.get_year()) + "_" + str(self.date)
     
-    def get_request(customer_in,brand_in,model_in,year_in,unixtimestamp):
+    def get_request(customer_in,brand_in,model_in,year_in,date):
         car_str = brand_in + " " + model_in + " " + str(year_in)
-        date = datetime.date.fromtimestamp(int(unixtimestamp))
         return Request.objects.get(customer=customer_in,car=car_str,date=date,status=False)
     
     def get_quantity(self):
         return self.quantity
+    
+    def get_date(self):
+        return self.date
     
     class Meta:
         db_table = 'request'
--- shop/views/editrequest.py
+++ shop/views/editrequest.py
@@ -27,8 +27,37 @@ class UpdateDeleteRequest(View):
         else:
             if button_action == "update" and quantity != 0:
                 arr = str(car_name).split()
-                return redirect('add-contract',customerusername=customer_name,brand=arr[0],model=arr[1],year=int(arr[2]),unixtimestamp=int(unixtimestamp))
+                return redirect('add-contract',customerusername=customer_name,brand=arr[0],model=arr[1],year=int(arr[2]),unixtimestamp=self.clean_date(unixtimestamp))
             if button_action == "delete" or quantity == 0:
                 Request.remove_request(str(car_name),customer_name)
             
         return redirect('view-cart')
+    
+    def clean_date(self,dt):
+        dt = str(dt).replace(',','')
+        arr = dt.split(' ')
+        if arr[0] == 'January':
+            arr[0] = '01'
+        if arr[0] == 'february':
+            arr[0] = '02'
+        if arr[0] == 'March':
+            arr[0] = '03'
+        if arr[0] == 'April':
+            arr[0] = '04'
+        if arr[0] == 'May':
+            arr[0] = '05'
+        if arr[0] == 'June':
+            arr[0] = '06'
+        if arr[0] == 'July':
+            arr[0] = '07'
+        if arr[0] == 'August':
+            arr[0] = '08'
+        if arr[0] == 'September':
+            arr[0] = '09'
+        if arr[0] == 'October':
+            arr[0] = '10'
+        if arr[0] == 'November':
+            arr[0] = '11'
+        if arr[0] == 'December':
+            arr[0] = '12'
+        return str(arr[2] + "-" + arr[0] + "-" + arr[1])
